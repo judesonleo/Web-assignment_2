d74119a119a2c69734ff4519855726a6a7d145e6
judesonleo <judesonleo123@gmail.com>
2024-02-17 12:37:54 +0530

    add video

--- mediaPlayer.js
+++ mediaPlayer.js
@@ -61,4 +61,81 @@ audio.addEventListener('timeupdate', () => {
 //     audio.currentTime = seekTime;
 // });
 
+// Video Section
 
+
+const video = document.getElementById('video');
+const videoplay = document.getElementById('videoplay');
+const video_range = document.getElementById('video-range');
+const mute = document.getElementById('muteBtn');
+const volume = document.getElementById('volume');
+const fullscreen = document.getElementById('fullscreen');
+
+
+videoplay.addEventListener('click',()=>{
+    if (video.paused || video.ended) {
+        video.play()
+        // play.innerHTML='<i class="fa-solid fa-pause"></i>'
+    } 
+    else {
+        video.pause()
+        
+        // play.innerHTML=`<i class="fa-solid fa-play"></i>`
+    }
+   })
+   video.addEventListener('play',()=>{
+    videoplay.innerHTML='<i class="fa-solid fa-pause"></i>'
+})
+video.addEventListener('pause',()=>{
+    videoplay.innerHTML='<i class="fa-solid fa-play"></i>'
+})
+video_range.addEventListener('input',()=>{
+    const value = video_range.value;
+    
+    const percent = (value - video_range.min)/(video_range.max - video_range.min) * 100;
+    console.log(percent);
+    // progress.style.background=`linear-gradient(to right, #4CAF50 0%, #4CAF50 ${percent}%,#d3d3d3 ${percent} ,#d3d3d3 100%)`;
+    video_range.style.background = `linear-gradient(to right, #4CAF50 ${percent}%, #d3d3d3 ${percent}%)`;
+
+    // const seekTime = (progress.value / 100) * audio.duration;
+    // audio.currentTime = seekTime;
+});
+video_range.addEventListener('input',()=>{
+    const seekTime = (video_range.value / 100) * video.duration;
+    video.currentTime = seekTime;
+})
+video.addEventListener('timeupdate', () => {
+    const currentTime = video.currentTime;
+    const duration = video.duration;
+    const progressPercentage = (currentTime / duration) * 100;
+    video_range.value = progressPercentage;
+
+    const percent = (video_range.value - video_range.min) / (video_range.max - video_range.min) * 100;
+    video_range.style.background = `linear-gradient(to right, #4CAF50 ${percent}%, #d3d3d3 ${percent}%)`;
+});
+
+muteBtn.addEventListener('click', () => {
+    if (video.muted) {
+      video.muted = false;
+      mute.innerHTML='<i class="fa-solid fa-volume-high"></i>'
+    } else {
+        video.muted = true;
+        mute.innerHTML='<i class="fa-solid fa-volume-xmark"></i>'
+    }
+  });
+  video.addEventListener('play',()=>{
+    mute.innerHTML='<i class="fa-solid fa-volume-high"></i>'
+  })
+  video.addEventListener('pause',()=>{
+    mute.innerHTML='<i class="fa-solid fa-volume-xmark"></i>'
+  })
+  volume.addEventListener('input', () => {
+    video.volume = volume.value;
+  });
+  fullscreen.addEventListener('click', () => {
+    if (video.requestFullscreen) {
+      video.requestFullscreen();
+    } else if (video.webkitRequestFullscreen) { /* Safari */
+      video.webkitRequestFullscreen();
+}
+  });
